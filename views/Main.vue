310366ff3fc02c5805b96045be29a68c17c2a73e
webistomin <webistomin@gmail.com>
2020-03-07 19:18:08 +0300

    feat(about): сделал блок about

--- views/Main.vue
+++ views/Main.vue
@@ -1,14 +1,15 @@
 <template lang="pug">
   AppContent
-    h1
+    AppAbout
 </template>
 
 <script>
   import AppContent from '@/components/partials/AppContent/AppContent';
+  import AppAbout from '@/components/ui/AppAbout/AppAbout';
 
   export default {
     name: 'Main',
-    components: { AppContent },
+    components: { AppAbout, AppContent },
     head() {
       return {
         title: 'Алексей Истомин :: Фронтенд разработчик',
